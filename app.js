660d37924e6dc659461638e4e72c70f2530eb52b
Victor Araujo Alves Da Silva <victor.alves@pgj.rj.gov.br>
2019-10-09 09:32:32 -0300

    varias alteracoes

--- app.js
+++ app.js
@@ -17,98 +17,165 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Driver
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "neo4j"));
-
-// Session
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "123456"));
 var session = driver.session();
 
-
-
-// Go Home route
-app.get('/', function (req, res) {
+// Home Route
+app.get('/',function(req, res){
     session
-        .run("MATCH (n:Person) RETURN n LIMIT 10")
-        .then(function (result) {
+        .run("MATCH (n:Person) RETURN n")
+        .then(function(result){
             var personArr = [];
-            result.records.forEach(function (record) {
-                // console.log(record._fields.);
+            
+            result.records.forEach(function(record){
+                //console.log(record._fields[0]);
                 personArr.push({
                     id: record._fields[0].identity.low,
                     name: record._fields[0].properties.name
                 });
             });
-
+            
             session
-                .run("MATCH (n:Movie) RETURN n LIMIT 10")
-                .then(function (resultMovie) {
-                    var movieArr = [];
-                    resultMovie.records.forEach(function (record) {
-                        movieArr.push(record._fields[0].properties)
+                .run("MATCH (n:Location) RETURN n")              
+                .then(function(result2){
+                    var locationArr = [];
+                    result2.records.forEach(function(record){
+                        locationArr.push(record._fields[0].properties);
                     });
+                    
                     res.render('index', {
                         persons: personArr,
-                        movies: movieArr
+                        locations: locationArr
                     });
                 })
-
+                   
         })
-        .catch(function (error) {
+        .catch(function(error){
             console.log(error);
-        })
-
+        });
 });
 
-// Add Person in route
+// Add Person Route
 app.post('/person/add', function(req, res){
     var name = req.body.name;
-    // console.log('Cad Ator Nome::', name);
-
+    
     session
-        .run("CREATE (n:Person {name:{nameParam}}) RETURN n.name", {nameParam: name})
+        .run("CREATE(n:Person {name:{nameParam}}) RETURN n.name", {nameParam: name})
         .then(function(result){
             res.redirect('/');
             session.close();
         })
         .catch(function(error){
-            console.log('Erro CREATE Person',error);
-        })
+            console.log(error);
+        });
 });
 
-// Add Movie in route
-app.post('/movie/add', function (req, res) {
-    var title = req.body.title;
-    var tagline = req.body.tagline;
-    var released = req.body.released;
 
+// Add Location Route
+app.post('/location/add', function(req, res){
+    var city = req.body.city;
+    var state = req.body.state;
+    
     session
-        .run("CREATE (n:Movie {title:{titleParam}, tagline:{taglineParam}, released:{releasedParam}}) RETURN n", { titleParam: title, taglineParam: tagline, releasedParam: released  })
-        .then(function (result) {
+        .run("CREATE(n:Location {city:{cityParam}, state:{stateParam}}) RETURN n", {cityParam: city, stateParam:state})
+        .then(function(result){
             res.redirect('/');
             session.close();
         })
-        .catch(function (error) {
-            console.log('Erro CREATE Person', error);
-        })
+        .catch(function(error){
+            console.log(error);
+        });
 });
 
-// Connect Actor in Movie Route
-app.post('/actor-movie/connect', function (req, res) {
-    var actor = req.body.actor;
-    var movie = req.body.movie;
-
+// Friends Connect Route
+app.post('/friends/connect', function(req, res){
+    var name1 = req.body.name1;
+    var name2 = req.body.name2;
+    var id = req.body.id;
+    
     session
-        .run("MATCH(a:Person {name: {actorParam}}),(b:Movie {title: {movieParam}}) MERGE (a)-[r:ACTED_IN]->(b) RETURN a, b", { actorParam: actor, movieParam: movie })
-        .then(function (result) {
-            res.redirect('/');
+        .run("MATCH(a:Person {name:{nameParam1}}),(b:Person {name:{nameParam2}}) MERGE(a)-[r:FRIENDS]->(b) RETURN a,b", {nameParam1: name1, nameParam2:name2})
+        .then(function(result){
+            if(id && id != null){
+                res.redirect('/person/'+id);
+            } else{
+                res.redirect('/');
+            }
             session.close();
         })
-        .catch(function (error) {
-            console.log('Erro CREATE Person', error);
-        })
+        .catch(function(error){
+            console.log(error);
+        });
 });
 
+// Add Birthplace Route
+app.post('/person/born/add', function(req, res){
+    var name = req.body.name;
+    var city = req.body.city;
+    var state = req.body.state;
+    var year = req.body.year;
+    var id = req.body.id;
+    
+    session
+        .run("MATCH(a:Person {name:{nameParam}}),(b:Location {city:{cityParam}, state:{stateParam}}) MERGE(a)-[r:BORN_IN {year:{yearParam}}]->(b) RETURN a,b", {nameParam: name, cityParam:city, stateParam:state,yearParam:year})
+        .then(function(result){
+            if(id && id != null){
+                res.redirect('/person/'+id);
+            } else{
+                res.redirect('/');
+            }
+            session.close();
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+});
 
+// Person Route
+app.get('/person/:id', function(req, res){
+    var id = req.params.id;
+    
+    session
+        .run("MATCH(a:Person) WHERE id(a)=toInt({idParam}) RETURN a.name as name", {idParam:id})
+        .then(function(result){
+            var name = result.records[0].get("name");
+            
+            session
+                .run("OPTIONAL MATCH (a:Person)-[r:BORN_IN]-(b:Location) WHERE id(a)=toInt({idParam}) RETURN b.city as city, b.state as state", {idParam:id})
+                .then(function(result2){
+                    var city = result2.records[0].get("city");
+                    var state = result2.records[0].get("state");
+                    
+                    session
+                        .run("OPTIONAL MATCH (a:Person)-[r:FRIENDS]-(b:Person) WHERE id(a)=toInt({idParam}) RETURN b", {idParam:id})
+                        .then(function(result3){
+                            var friendsArr = [];
+                            
+                            result3.records.forEach(function(record){
+                                if(record._fields[0] != null){
+                                    friendsArr.push({
+                                        id: record._fields[0].identity.low,
+                                        name: record._fields[0].properties.name
+                                    });
+                                }
+                            });
+                            
+                            res.render('person',{
+                                id:id,
+                                name:name,
+                                city:city,
+                                state: state,
+                                friends:friendsArr
+                            });
+                            
+                            session.close();
+                        })
+                        .catch(function(error){
+                            console.log(error);
+                        });
+                });
+        });
+});
 
 app.listen(3000);
 
